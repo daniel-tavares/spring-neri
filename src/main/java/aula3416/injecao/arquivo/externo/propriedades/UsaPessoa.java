c6e96ba93fce185f6196391f3763c0ede6363764
daniel <daniel.exatas@gmail.com>
2017-02-26 19:59:04 -0300

    Outra versão para buscar propriedades em arquivos externos utilizando o xmlns:context

--- src/main/java/aula3416/injecao/arquivo/externo/propriedades/UsaPessoa.java
+++ src/main/java/aula3416/injecao/arquivo/externo/propriedades/UsaPessoa.java
@@ -5,7 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class UsaPessoa {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("3416ApplicationContext.xml");
+
+		// O arquivo 3416ApplicationContext.xml também é para o mesmo proposito,
+		// porem com o arquivo do ApplicationContext modificado, a forma abaixo
+		// é utilizando o xmlns:context
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("3417ApplicationContext.xml");
 
 		BeanPessoa beanPessoa = context.getBean("beanPessoa", BeanPessoa.class);
 
