49f617989dc3b9881eb2cf25dbe88e4dfcc0499a
daniel <daniel.exatas@gmail.com>
2017-02-26 19:43:32 -0300

    Trabalhando com arquivos properties no Spring

---
+++ src/main/java/aula3416/injecao/arquivo/externo/propriedades/BeanPessoa.java
@@ -0,0 +1,15 @@
+package aula3416.injecao.arquivo.externo.propriedades;
+
+public class BeanPessoa {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
