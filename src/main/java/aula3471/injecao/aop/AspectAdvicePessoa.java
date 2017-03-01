0c37cb185cf85879de252e9c629ac1edb5052df6
daniel <daniel.exatas@gmail.com>
2017-03-01 18:49:20 -0300

    Testes com @Before e @After da POA

--- src/main/java/aula3471/injecao/aop/AspectAdvicePessoa.java
+++ src/main/java/aula3471/injecao/aop/AspectAdvicePessoa.java
@@ -1,5 +1,6 @@
 package aula3471.injecao.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -7,11 +8,29 @@ import org.aspectj.lang.annotation.Before;
 public class AspectAdvicePessoa {
 
 	// Simbolos para PointCuts = . << ponto ex: grava. = gravaA ou gravaB etc...
-	// Simbolos para PointCuts = + << mais ex: grava.+ = gravaCliente ou gravaBairro etc... [mas nao com grava]
-	// Simbolos para PointCuts = * << vezes ex: grava.* = grava gravaA ou gravaB etc...
-	// Simbolos para PointCuts = \ << barra ex: \grava. = cliente.grava  [mas nao com grava]
+	// Simbolos para PointCuts = + << mais ex: grava.+ = gravaCliente ou
+	// gravaBairro etc... [mas nao com grava]
+	// Simbolos para PointCuts = * << vezes ex: grava.* = grava gravaA ou gravaB
+	// etc...
+	// Simbolos para PointCuts = \ << barra ex: \grava. = cliente.grava [mas nao
+	// com grava]
 	@Before("execution(public * *(..))")
-	public void manipulouDados() {
-		System.out.println("manipulouDados()");
+	public void manipulouDadosBefore() {
+		System.out.println("\nmanipulouDadosBefore()");
+	}
+
+	@After("execution(public * *(..))")
+	public void manipulouDadosAfter() {
+		System.out.println("manipulouDadosAfter()");
+	}
+
+	@After("execution(public * consulta*(..))")
+	public void afterConsultas() {
+		System.out.println("Consulta");
+	}
+
+	@After("execution(public * consultaPessoaPeloCodigo(..))")
+	public void afterConsultasPorCodigo() {
+		System.out.println("Consultou por codigo");
 	}
 }
